b326466a12db981762fe4c8e93fb59566dbbc657
Dmitry Rodiontsev <dmitry.rodiontsev@gmail.com>
2011-06-22 23:48:14 +0400

    ProjectInfo extracted into independent service

---
+++ src/main/java/com/rodiontsev/tools/maven/plugins/ProjectInfoProvider.java
@@ -0,0 +1,10 @@
+package com.rodiontsev.tools.maven.plugins;
+
+/**
+ * Date: 22.06.11
+ * Time: 23:21
+ *
+ * @author <a href="http://www.rodiontsev.com">Dmitry Rodiontsev</a>
+ */
+public class ProjectInfoProvider {
+}
